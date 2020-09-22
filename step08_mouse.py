26cfc40f80938c46e9c62738c563c1627de9e375
chaeyeonp <chaeyeon.park@outlook.com>
2020-09-22 10:59:46 +0900

    Mouse Event Prac

---
+++ step08_mouse.py
@@ -0,0 +1,18 @@
+import sys
+import numpy as np
+import cv2
+
+#마우스 움직임에 따라 그림 그릴 수 있는 사용자 정의 함수
+def on_mouse():
+    print("마우스 이벤트 처리")
+
+
+#도화지
+img = np.ones((480,670,3),dtype=np.uint8) * 255
+
+#영상에 개별 이름 설정
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',on_mouse, img)
+cv2.imshow('image', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
