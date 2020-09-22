14a602cbc7f939658b3c18b986cceb567d3d0ba4
chaeyeonp <chaeyeon.park@outlook.com>
2020-09-22 10:38:03 +0900

    keyboard image 반전

--- step07_keyboard.py
+++ step07_keyboard.py
@@ -5,3 +5,24 @@ import glob
 
 #i or I를 입력시에 이미지가 반전
 # ~틸트: 반전 연산자
+
+img = cv2.imread('imglist/cat.bmp',cv2.IMREAD_GRAYSCALE)
+
+if img is None:
+    print('image 로딩 실패')
+    sys.exit()
+
+cv2.imshow('image',img)
+
+while True:
+    keycode = cv2.waitKey()
+    if keycode == ord('i') or keycode == ord('I'):
+        img = ~img
+        cv2.imshow('image',img)
+
+
+    elif keycode == 27:
+        break
+
+
+cv2.destroyAllWindows()
